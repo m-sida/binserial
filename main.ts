9addbfae6499fae405666abe010314f0e84c49d9
m-sida <146202085+m-sida@users.noreply.github.com>
2023-09-28 14:43:04 +0900

    delete un-necessary quotation.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\uf075" color=#008080" block="sample"
+//% weight=70 icon="\uf075" color=#008080 block="sample"
 namespace comment {
     //% blockId=show_strings block="sample %v"
     export function noaction(text: string): void {
