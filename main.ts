8808f063c5a71cb7b35de6da4d735c2b2e940f9b
m-sida <146202085+m-sida@users.noreply.github.com>
2023-09-28 14:14:46 +0900

    add sample of main.ts

---
+++ main.ts
@@ -0,0 +1,6 @@
+//% weight=70 icon="\uf075" color=#008080" block="sample"
+namespace comment {
+    //% blockId=show_strings block="sample %v"
+    export function noaction(text: string): void {
+    }
+}
