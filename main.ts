c111f7446f26bf729c019edebf1c0f5271e1087a
m-sida <146202085+m-sida@users.noreply.github.com>
2023-10-14 14:51:43 +0900

    fix number of operand

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ namespace binserial {
         }
         serial.writeString(chex);
     }
-    //% blockId=basic block="binserial_setport %v"
+    //% blockId=basic block="binserial_setport %v %v"
     //% shim=bserial::binserial_setport
     export function binserial_setport(tx: number, rx: number): void {
     }
