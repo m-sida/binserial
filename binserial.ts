35f9bf90ca3111bfaf247517cdfe9413e7784893
m-sida <146202085+m-sida@users.noreply.github.com>
2023-10-15 16:35:46 +0900

    fix block parameter

--- binserial.ts
+++ binserial.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf044" color=#008080 block="binserial"
 namespace bserial {
-    //% blockId=binserialtx block="binserialtx %v"
+    //% blockId=binserialtx block="binserialtx %hex"
     export function binserialtx(hex: number): void {
         let chex;
         if (hex == 0) {
@@ -10,18 +10,18 @@ namespace bserial {
         }
         serial.writeString(chex);
     }
-    //% blockId=binserial_redirect block="binserial_redirect %v %v"
+    //% blockId=binserial_redirect block="binserial_redirect %tx %rx"
     //% shim=bcserial::binserial_redirect
     export function binserial_redirect(tx: number, rx: number): void {
         //
     }
-    //% blockId=binserial_sendchar block="binserial_sendchar %v %v"
+    //% blockId=binserial_sendchar block="binserial_sendchar %txchar %mode"
     //% shim=bcserial::binserial_sendchar
     export function binserial_sendchar(txchar: string, mode: number): number {
         // bserial::binserial_sendchar(txchar, mode);
         return 0;
     }
-    //% blockId=binserial_send block="binserial_send %v"
+    //% blockId=binserial_send block="binserial_send %txchar"
     //% shim=bcserial::binserial_send
     export function binserial_send(txchar: number): number {
         // bserial::binserial_send(txchar);
