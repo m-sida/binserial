afd39d9d43beb3691417263e47ec3586900905f4
m-sida <146202085+m-sida@users.noreply.github.com>
2023-10-22 21:32:04 +0900

    update new test code

--- test.ts
+++ test.ts
@@ -1,16 +1,15 @@
 // テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
-enum MicroBitSerialMode
-{
-    ASYNC,
-    SYNC_SPINWAIT,
-    SYNC_SLEEP
-}
-serial.redirect(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BaudRate115200)
-//bserial.binserial_redirect(SerialPin.USB_TX, SerialPin.USB_RX)
-basic.forever(() => {
-    //bserial.binserialtx(0)
-    //bserial.binserial_sendchar("7", MicroBitSerialMode.SYNC_SLEEP)
-    //let ss: string = String.fromCharCode(60)
-    //bserial.binserial_send(ss)
-    bserial.binserial_write(0)
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 256; index++) {
+        bserial.binserial_write(txsend)
+        txsend += 1
+        basic.pause(10)
+    }
 })
+let txsend = 0
+serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+)
+txsend = 0
